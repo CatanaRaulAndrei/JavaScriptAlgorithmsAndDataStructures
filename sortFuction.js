b0b4a1aba0cb69aad86a1070ae96e9a833edf0a4
Raul <raul19@windowslive.com>
2021-03-14 19:09:56 +0200

    Update sortFuction.js

--- sortFuction.js
+++ sortFuction.js
@@ -1,15 +1,12 @@
-// Sort function sort letters, but don't sort numbers
-// Example on letters:
 
+// Example on letters:
 let letters = ['a','b','c','d','e','f' ,'g', 'h' ,'i', 'j', 'k' ,'l', 'm', 'n' ,'o', 'p' ,'q', 'r', 's', 't', 'u' ,'v', 'w' ,'x' ,'y' ,'z'];
 let reversedArray = letters.reverse();
 
-console.log("Reversed: " + reversedArray);
 console.log("Sorted: "+reversedArray.sort());
 
 // Example for numbers
 let numbers = [12,8908097,4544,121,78678,44,9,0,1,-100];
-
 let correctSort = numbers.sort((a,b)=>a-b);
 
 console.log("Correct Sort: "+ correctSort);
