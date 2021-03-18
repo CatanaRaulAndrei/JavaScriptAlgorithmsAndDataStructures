df1e40026948915db9fb519b7e0a5ad73d38236a
Raul <raul19@windowslive.com>
2021-03-18 22:06:17 +0200

    Update UndirectedGraph.js

--- UndirectedGraph.js
+++ UndirectedGraph.js
@@ -63,3 +63,12 @@ myGraph.addEdge('6','5');
 myGraph.showConnections();
 
 // This graph is represented with Adjacency list
+/*
+0-->1 2 
+1-->3 2 0 
+2-->4 1 0 
+3-->1 4 
+4-->3 2 5 
+5-->4 6 
+6-->5 
+*/
